99933f34d57fca4b95c77548cdd4c49ab1d28911
VasuNVerma <vermavasun@gmail.com>
2022-10-10 01:28:00 -0500

    First Commit - Intital structures

---
+++ users/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+def login_user(request):
+    return render(request, 'authenticate/login.html', {})
