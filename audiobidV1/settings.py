cf8647e5e612a54104de0cc9932362c7ccd15e20
Vasu Naman Verma <vermavasun@gmail.com>
2022-10-20 01:36:12 -0500

    Changes for Heroku Setup

--- audiobidV1/settings.py
+++ audiobidV1/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 import configparser
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,9 @@ config.read('config.ini')
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config['psql']['secret']
+
+environ.Env.read_env()
+SECRET_KEY = os.environ['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
