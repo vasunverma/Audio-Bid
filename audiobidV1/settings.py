1a509f508ca229f00fe857c81a11faeb510d1700
Vasu Naman Verma <vermavasun@gmail.com>
2022-10-20 12:27:44 -0500

    Settings Update

--- audiobidV1/settings.py
+++ audiobidV1/settings.py
@@ -25,7 +25,7 @@ environ.Env.read_env()
 SECRET_KEY = os.environ['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'audiobid.herokuapp.com',
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'mainApp',
     'users',
-
     'crispy_forms',
     'allauth',
     'allauth.account',
@@ -126,11 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, 'static'),
-]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "static/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
