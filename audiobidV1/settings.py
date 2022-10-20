46d1ad1b27ecbfc1f5e9f80320b9353ced0fdaf3
Vasu Naman Verma <vermavasun@gmail.com>
2022-10-20 12:49:38 -0500

    Update

--- audiobidV1/settings.py
+++ audiobidV1/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
