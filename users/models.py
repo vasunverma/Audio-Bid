eaf261334018a15a4de0168d24c0fb76319702fc
Vasu Naman Verma <vermavasun@gmail.com>
2022-10-19 23:45:41 -0500

    Cleanup

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=10, default='worker', null=False) # Need to change default thing later and prompt user to select a user type on first login
+    role = models.CharField(max_length=10) # Need to change default thing later and prompt user to select a user type on first login
     native_auth = models.BooleanField(default=False)
 
     def __str__(self):
