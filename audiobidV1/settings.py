54c6bf9147e2bb10d205797d65240f89c8d0d3f0
Vasu Naman Verma <vermavasun@gmail.com>
2022-10-20 03:49:32 -0500

    Updated Allowed Hosts to Add Heroku URL

--- audiobidV1/settings.py
+++ audiobidV1/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = os.environ['secret_key']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'audiobid.herokuapp.com'
+]
 
 # Application definition
 INSTALLED_APPS = [
