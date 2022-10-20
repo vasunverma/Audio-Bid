ba48a8d99857cb03240dbcec11214867c6170e92
Vasu Naman Verma <vermavasun@gmail.com>
2022-10-20 03:07:46 -0500

    CollectStatic Error Fix

--- audiobidV1/settings.py
+++ audiobidV1/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_URL = 'static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
