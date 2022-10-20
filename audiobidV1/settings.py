6f92376eb58935cc4e3efb48ef521769e0a41a02
Vasu Naman Verma <vermavasun@gmail.com>
2022-10-20 18:17:54 -0500

    Debug Flag

--- audiobidV1/settings.py
+++ audiobidV1/settings.py
@@ -157,7 +157,7 @@ ACCOUNT_LOGOUT_ON_GET = True
 ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_EMAIL_REQUIRED = True
 
-SITE_ID = 2  # Keeping on incrementing this if you get an error while using google auth
+SITE_ID = 1  # Keeping on incrementing this if you get an error while using google auth
 
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
