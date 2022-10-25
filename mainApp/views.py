97c4bb1646e5217ada926752fa51aa8a1ef4cda5
ajinkyamalhotra1 <113081549+ajinkyamalhotra1@users.noreply.github.com>
2022-10-24 22:11:32 -0500

    Adding Finish Profile pop-up on user_login/home_page if we are missing the required user data in DB

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,13 +1,42 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.contrib import messages
+import pytz
 
 
 def home(request):
     if request.user.is_authenticated:
-        if User.objects.get(id=request.user.id).profile.role == ''\
-                or User.objects.get(id=request.user.id).profile.time_zone == '':
-            messages.success(request, 'Please fill out the missing info')
-            return redirect('users/profile')
+        user = User.objects.get(id=request.user.id)
+        if request.method == 'GET':
+            if User.objects.get(id=request.user.id).profile.role == ''\
+                    or User.objects.get(id=request.user.id).profile.time_zone == '':
 
-    return render(request, 'home.html', {})
+                selectedTimeZone = "UTC"
+                if user.profile.time_zone != '':
+                    selectedTimeZone = user.profile.time_zone
+
+                selectedRoleType = user.profile.role
+                if selectedRoleType == '':
+                    selectedRoleType = 'worker'
+
+                nonSelectedRoleType = 'creator'
+                if selectedRoleType == 'creator':
+                    nonSelectedRoleType = 'worker'
+
+                return render(request, 'home.html', {"MissingInfo": True,
+                                                     "fname": user.first_name,
+                                                     "lname": user.last_name,
+                                                     "username": user.username,
+                                                     "email": user.email,
+                                                     "selectedRoleType": selectedRoleType,
+                                                     "nonSelectedRoleType": nonSelectedRoleType,
+                                                     "selectedTimeZone": selectedTimeZone,
+                                                     'timezones': pytz.common_timezones})
+
+        elif request.method == 'POST':
+            user.profile.role = request.POST.get('inlineRadioOptions')
+            user.profile.time_zone = request.POST.get('userTimezone')
+            user.save()
+
+            render(request, 'home.html', {"MissingInfo": False})
+
+    return render(request, 'home.html', {"MissingInfo": False})
