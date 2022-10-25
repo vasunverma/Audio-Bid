d5635a68b485c9cb012c7f7116c54d1049ad4507
Vasu Naman Verma <vermavasun@gmail.com>
2022-10-25 12:49:22 -0500

    Site ID Incremented

--- audiobidV1/settings.py
+++ audiobidV1/settings.py
@@ -168,7 +168,7 @@ ACCOUNT_LOGOUT_ON_GET = True
 ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_EMAIL_REQUIRED = True
 
-SITE_ID = 3  # Keeping on incrementing this if you get an error while using google auth
+SITE_ID = 5  # Keeping on incrementing this if you get an error while using google auth
 
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
