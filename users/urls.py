b426f0765b92ca23a22c093e5270dd9a210a5fcb
Vasu Naman Verma <vermavasun@gmail.com>
2022-11-01 17:51:47 -0500

    Forgot Password Functionality

--- users/urls.py
+++ users/urls.py
@@ -3,10 +3,12 @@ from . import views
 
 urlpatterns = [
     path('login', views.user_login, name='login_url'),
+    path("logout", views.user_logout, name= "logout"),
     path('signup', views.users_signup, name='signup_url'),
     path('profile', views.users_profile, name='profile_url'),
     path('jobs/', views.users_jobs, name='jobs_url'),
     path('Reviews/', views.users_reviews, name='reviews_url'),
     path('Payments/', views.users_payments, name='payments_url'),
     path('instructions/', views.users_instructions, name='instructions_url'),
+    path("reset_password", views.users_reset_password, name="password_reset"),
 ]
--- users/views.py
+++ users/views.py
@@ -3,11 +3,21 @@ import datetime
 import pytz
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from django.core.files.storage import default_storage
 from .forms import JobForm
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.models import User
+from django.template.loader import render_to_string
+from django.db.models.query_utils import Q
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
 
 def user_login(request):
     form = AuthenticationForm()
@@ -33,6 +43,9 @@ def user_login(request):
 
     return render(request, 'registration/login.html', {'form': form, 'title': 'log in'})
 
+def user_logout(request):
+    logout(request)
+    return redirect('home')
 
 def users_signup(request):
     if request.method == 'POST':
@@ -77,6 +90,35 @@ def users_signup(request):
         messages.error(request, "Account not created, please try again!")
         return render(request, 'registration/signup.html', {"error": error})
 
+def users_reset_password(request):
+    if request.method == "POST":
+        password_reset_form = PasswordResetForm(request.POST)
+        if password_reset_form.is_valid():
+            data = password_reset_form.cleaned_data['email']
+            associated_users = User.objects.filter(Q(email=data))
+            if associated_users.exists():
+                for user in associated_users:
+                    subject = "Password Reset Requested"
+                    email_template_name = "registration/password/password_reset_email.txt"
+                    c = {
+                        "email":user.email,
+                        'domain':'127.0.0.1:8000',
+                        'site_name': 'Website',
+                        "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                        "user": user,
+                        'token': default_token_generator.make_token(user),
+                        'protocol': 'http',
+                    }
+                    email = render_to_string(email_template_name, c)
+                    try:
+                        send_mail(subject, email, 'admin@example.com' , [user.email], fail_silently=False)
+                    except BadHeaderError:
+                        messages.error(request, "Error occurred while sending reset email.")
+                        return render(request, 'registration/password/password_reset.html')
+                    messages.success(request, 'A message with reset password instructions has been sent to your inbox.')
+                    return render(request, 'registration/password/password_reset.html')
+    password_reset_form = PasswordResetForm()
+    return render(request=request, template_name="registration/password/password_reset.html", context={"password_reset_form":password_reset_form})
 
 def users_profile(request):
     if request.user.is_authenticated:
