3b78d99d38cfa7f54f9ddec9e95b999286b550d6
Vasu Naman Verma <vermavasun@gmail.com>
2022-11-13 02:03:12 -0600

    Edit and Create Job on Job Page per Job

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("logout", views.user_logout, name= "logout"),
     path('signup', views.users_signup, name='signup_url'),
     path('profile', views.users_profile, name='profile_url'),
+    path("jobs/<int:id>/", views.users_edit_job, name="edit_job_url"),
     path('jobs/', views.users_jobs, name='jobs_url'),
     path('Reviews/', views.users_reviews, name='reviews_url'),
     path('Payments/', views.users_payments, name='payments_url'),
