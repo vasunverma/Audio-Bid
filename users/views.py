9bcfcba0af8eb281df783ba56e9bb8517a7a91dd
ajinkyamalhotra1 <113081549+ajinkyamalhotra1@users.noreply.github.com>
2022-11-13 02:28:16 -0600

    Adding Claim/Delete/Cancel/Download Audio functionality

--- users/views.py
+++ users/views.py
@@ -163,12 +163,26 @@ def users_jobs(request):
     if request.user.is_authenticated:
         if request.method == 'POST':
             if request.POST.get("formId") == "claimJobform":
-                job_id = request.POST.get("jobIdToBeClaimed")
-                job_to_be_claimed = Job.objects.get(id=job_id)
-                job_to_be_claimed.worker_id = request.user.id
-                job_to_be_claimed.status = 1
-                job_to_be_claimed.save()
+                job_id = request.POST.get("jobId")
+                job = Job.objects.get(id=job_id)
+                job.worker_id = request.user.id
+                job.status = 1
+                job.save()
                 messages.success(request, ('Your Job has been successfully claimed!'))
+
+            elif request.POST.get("formId") == "cancelJobform":
+                job_id = request.POST.get("jobId")
+                job = Job.objects.get(id=job_id)
+                job.worker_id = 0
+                job.status = 0
+                job.save()
+                messages.success(request, ('Your Job has been successfully cancelled!'))
+
+            elif request.POST.get("formId") == "deleteJobform":
+                job_id = request.POST.get("jobId")
+                job = Job.objects.get(id=job_id)
+                job.delete()
+                messages.success(request, ('Your Job has been successfully deleted!'))
 
             else:
                 form = JobForm(request.POST or None)
@@ -226,7 +240,9 @@ def users_jobs(request):
             return render(request, 'jobs/jobs.html', {'page': page,
                                                       'posts': posts,
                                                       'myFilter': myFilter,
-                                                      'creator': is_creator})
+                                                      'creator': is_creator,
+                                                      'user_id_str': str(request.user.id),
+                                                      'user_id': request.user.id})
     else:
         return redirect('login_url')
 
