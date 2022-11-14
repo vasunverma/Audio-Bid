c7bfc058169c8f7c6668f680451b55c8f05ed0d1
Neel Akash <neelakash@tamu.edu>
2022-11-14 07:36:28 -0600

    Added pause resume functionality

--- users/static/js/edit-job.js
+++ users/static/js/edit-job.js
@@ -1,8 +1,7 @@
 URL = window.URL || window.webkitURL;
 var gumStream, rec, input, audioContext;
 var AudioContext = window.AudioContext || window.webkitAudioContext;
-
-
+var startEdit, pauseEdit, resumeEdit, stopEdit, redEdit, recordingEdit, recordedEdit;
 
 Date.prototype.toDateInputValue = (function() {
     var local = new Date(this);
@@ -17,12 +16,12 @@ function getSubpath(path, subpathLevel) {
   subpath = "";
   for(i = 0; i < subpathLevel && i < arr.length; i++)
       subpath += "/" + arr[i+1];
-  return subpath;S
+  return subpath;
 }
 
 $('.drop-down-show-hide').hide();
 
-$('#editDropDown').change(function () {
+$('#editDropDown').change(function() {
     $('.drop-down-show-hide').hide()    
     $('#' + this.value).show();
     if(this.value == "div1"){
@@ -59,46 +58,44 @@ $(".btn-modal").on('click',function(e){
     url: "/users/jobs/"+id+"/",
     type:'GET',
     datatype: 'json',
-    success: function(data){
+    success: function(data) {
         $('#edit-name').val(data["name"])
         $('#edit-description').val(data["description"])
         $('#edit-price').val(data["price"])
         $('#edit_end_date').val(data["end_date"])
         $('#cardModal').modal("show");
-        },  
-    error:function()
-        {
+    },  
+    error:function() {
         alert("Some error occurred while trying to update the job. Please try again in sometime.");
         window.location.href = path; 
-        }
-
-    });
+    }});
 
 });
 
-
-$(".editSaveBtn").on('click',function(e){
+$(".editSaveBtn").on('click', function(e) {
     idd = $(this).attr('id')
     $("#edit-form").attr('action', "/users/jobs/"+idd+"/");
     $("#edit-form").attr('method', 'post'); 
 });
 
+redEdit = document.getElementById("edit-red-dot");
+startEdit = document.getElementById("edit-start-rec");
+pauseEdit = document.getElementById("edit-pause-rec");
+resumeEdit = document.getElementById("edit-resume-rec");
+stopEdit = document.getElementById("edit-stop-rec");
+recordingEdit = document.getElementById("edit-record-file");
+recordedEdit = document.getElementById("edit-recorded");
 
-var edit_red = document.getElementById("edit-red-dot");
-var edit_start = document.getElementById("edit-start-rec");
-var edit_stop = document.getElementById("edit-stop-rec");
-var edit_recording = document.getElementById("edit-record-file");
-var edit_recorded = document.getElementById("edit-recorded");
+startEdit.addEventListener("click", startEditRecording);
+pauseEdit.addEventListener("click", pauseEditRecording);
+resumeEdit.addEventListener("click", resumeEditRecording);
+stopEdit.addEventListener("click", stopEditRecording);
 
-edit_start.addEventListener("click", editstartRecording);
-edit_stop.addEventListener("click", editstopRecording);
+// Audio Recorder functions
 
-function editstartRecording() {
-    console.log("STARTING");
-    edit_start.style.display = "none";
-    edit_stop.style.display = "block";
-    edit_red.style.display = "block";
-    edit_recording.src = "";
+function startEditRecording() {
+    handleEditButtons(false, true, false, true, true);
+    recordingEdit.src = "";
 
     var constraints = { audio: true, video: false }
     navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
@@ -108,28 +105,47 @@ function editstartRecording() {
 		rec = new Recorder(input,{numChannels:1});
 		rec.record();
 	}).catch(function(err) {
+        handleEditButtons(true, false, false, false, false);
         console.log("ERROR", err);
 	});
 }
 
-function editstopRecording() {
-    console.log("STOPPING");
-    edit_start.style.display = "block";
-    edit_stop.style.display = "none";
-    edit_red.style.display = "none";
-
-    rec.stop();
-	gumStream.getAudioTracks()[0].stop();
-	rec.exportWAV(edithandleBlob);
+function pauseEditRecording() {
+    handleEditButtons(false, false, true, true, false);
+    if (rec.recording) {
+        rec.stop();
+    }
 }
 
-function edithandleBlob(blob) {
+function resumeEditRecording() {
+    handleEditButtons(false, true, false, true, true);
+    if (!rec.recording) {
+        rec.record();
+    }
+}
+
+function stopEditRecording() {
+    handleEditButtons(true, false, false, false, false);
+    rec.stop();
+	gumStream.getAudioTracks()[0].stop();
+	rec.exportWAV(handleEditBlob);
+}
+
+function handleEditBlob(blob) {
     let url = URL.createObjectURL(blob);
-    edit_recording.src = url;
+    recordingEdit.src = url;
 
     let file = new File([blob], "test.wav", {type:"audio/wav", lastModified:new Date().getTime()});
     let container = new DataTransfer();
     container.items.add(file);
-    edit_recorded.files = container.files;
+    recordedEdit.files = container.files;
+}
+
+function handleEditButtons(x1, x2, x3, x4, x5) {
+    startEdit.style.display = x1?"block":"none";
+    pauseEdit.style.display = x2?"block":"none";
+    resumeEdit.style.display = x3?"block":"none";
+    stopEdit.style.display = x4?"block":"none";
+    redEdit.style.display = x5?"inline":"none";
 }
 
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 import os
+import re
 import requests
 import datetime
 import pytz
@@ -323,7 +324,8 @@ def check_gdrive(url):
 
 
 def filename_gen(user, ext):
-    return str(datetime.datetime.now().timestamp()).replace('.', '-') + "-" + user + ext
+    u = re.sub('\W+','', user)
+    return str(datetime.datetime.now().timestamp()).replace('.', '-') + "-" + u + ext
 
 
 def url_gen(filename):
