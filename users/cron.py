29fdec0a5572fa070bfe9620cae62e3c5ee59cc3
Vasu Naman Verma <vermavasun@gmail.com>
2022-12-09 18:43:20 -0600

    Cron Job Change

--- users/cron.py
+++ users/cron.py
@@ -1,19 +1,20 @@
 from datetime import datetime
 from users.models import Job
+from django.db.models.query_utils import Q
 
 def dynamic_job_price():
-    jobs = Job.objects.filter(Q(status= 0))
+    jobs = Job.objects.filter(Q(status= 0)).order_by("id")
     for job in jobs:
         if job.limit_price != 0 and job.price < job.limit_price:
-            dayPassed = (datetime.datetime.utcnow().date() - job.created_date.date()).days
+            dayPassed = (datetime.utcnow().date() - job.created_date.date()).days
             if job.cron_date is None and dayPassed == 1:
-                job.cron_date = datetime.datetime.now();
+                job.cron_date = datetime.now();
                 job.price = job.price + 1
                 job.save()
             else:
-                dayPassed = (job.cron_date - job.created_date.date()).days
+                dayPassed = (job.cron_date.date() - job.created_date.date()).days
                 if dayPassed == 1:
-                    job.cron_date = datetime.datetime.now();
+                    job.cron_date = datetime.now();
                     job.price = job.price + 1
                     job.save()
 
